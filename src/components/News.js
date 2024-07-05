8ff8a90954fa87bdf55098636b6046f1eb745c5b
Akshay Viswanathan <akshayviswa9876@gmail.com>
2024-07-05 12:41:29 +0530

    added files

--- src/components/News.js
+++ src/components/News.js
@@ -1,3 +1,118 @@
+// import React, { useEffect, useState } from 'react';
+// import NewsItem from './NewsItem';
+// import Spin from './Spin';
+// import PropTypes from 'prop-types';
+
+// const News = (props) => {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [page, setPage] = useState(1);
+//   const [totalResults, setTotalResults] = useState(0);
+
+//   const capitalize = (string) => {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//   };
+
+//   const updateNews = async () => {
+//     props.setProgress(10);
+//     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+
+//     setLoading(true);
+//     const data = await fetch(url);
+//     const parsedData = await data.json();
+//     setArticles(parsedData.articles);
+//     setTotalResults(parsedData.totalResults);
+//     setLoading(false);
+//     props.setProgress(100);
+//   };
+
+//   useEffect(() => {
+//     document.title = `${capitalize(props.category)} - VishwaSamachar`;
+//     updateNews();
+//     // eslint-disable-next-line
+//   }, [page]);
+
+//   const handlePrevClick = async () => {
+//     setPage(page - 1);
+//   };
+
+//   const handleNextClick = async () => {
+//     if (page + 1 <= Math.ceil(totalResults / props.pageSize)) {
+//       setPage(page + 1);
+//     }
+//   };
+
+//   return (
+//     <div className="container my-3">
+//       <h1 className="text-center " style={{margin: '35px 0px',marginTop:'90px'}}>
+//         <b>
+//           <i>
+//             <span style={{ color: 'red' }}>
+//               VishwaSamachar-Top headlines on {capitalize(props.category)}
+//             </span>
+//           </i>
+//         </b>
+//       </h1>
+
+//       {loading && <Spin />}
+//       <div className="row">
+//         {!loading &&
+//           articles.map((element) => {
+//             return (
+//               <div className="col-md-4" key={element.url}>
+//                 <NewsItem
+//                   title={element.title ? element.title.slice(0, 45) : ''}
+//                   description={
+//                     element.description ? element.description.slice(0, 88) : ''}
+//                   imageUrl={element.urlToImage}
+//                   newsUrl={element.url}
+//                   author={element.author}
+//                   date={element.publishedAt}
+//                   source={element.source.name}
+//                 />
+//               </div>
+//             );
+//           })}
+//       </div>
+//       <div className="container d-flex justify-content-between">
+//         <button
+//           disabled={page <= 1}
+//           type="button"
+//           className="btn btn-dark"
+//           onClick={handlePrevClick}
+//         >
+//           &larr; Previous
+//         </button>
+//         <button
+//           disabled={page + 1 > Math.ceil(totalResults / props.pageSize)}
+//           type="button"
+//           className="btn btn-dark"
+//           onClick={handleNextClick}
+//         >
+//           Next &rarr;
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// News.defaultProps = {
+//   country: 'in',
+//   pageSize: 6,
+//   category: 'general',
+// };
+
+// News.propTypes = {
+//   country: PropTypes.string,
+//   pageSize: PropTypes.number,
+//   category: PropTypes.string,
+//   apiKey: PropTypes.string.isRequired,
+//   setProgress: PropTypes.func.isRequired,
+// };
+
+// export default News;
+
+
 import React, { useEffect, useState } from 'react';
 import NewsItem from './NewsItem';
 import Spin from './Spin';
@@ -15,7 +130,9 @@ const News = (props) => {
 
   const updateNews = async () => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    
+    // Update URL to use your deployed API
+    const url = `https://projectapi-dfv8.onrender.com/news?country=${props.country}&category=${props.category}&page=${page}&pageSize=${props.pageSize}`;
 
     setLoading(true);
     const data = await fetch(url);
@@ -30,7 +147,7 @@ const News = (props) => {
     document.title = `${capitalize(props.category)} - VishwaSamachar`;
     updateNews();
     // eslint-disable-next-line
-  }, [page]);
+  }, [page, props.category, props.country]);
 
   const handlePrevClick = async () => {
     setPage(page - 1);
@@ -44,11 +161,11 @@ const News = (props) => {
 
   return (
     <div className="container my-3">
-      <h1 className="text-center " style={{margin: '35px 0px',marginTop:'90px'}}>
+      <h1 className="text-center " style={{ margin: '35px 0px', marginTop: '90px' }}>
         <b>
           <i>
             <span style={{ color: 'red' }}>
-              VishwaSamachar-Top headlines on {capitalize(props.category)}
+              VishwaSamachar - Top headlines on {capitalize(props.category)}
             </span>
           </i>
         </b>
@@ -63,7 +180,8 @@ const News = (props) => {
                 <NewsItem
                   title={element.title ? element.title.slice(0, 45) : ''}
                   description={
-                    element.description ? element.description.slice(0, 88) : ''}
+                    element.description ? element.description.slice(0, 88) : ''
+                  }
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
                   author={element.author}
@@ -106,8 +224,8 @@ News.propTypes = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
   category: PropTypes.string,
-  apiKey: PropTypes.string.isRequired,
   setProgress: PropTypes.func.isRequired,
 };
 
 export default News;
+
