95da6b1d68f26ff7effd8aef75e00d993e5fcfd0
Akshay Viswanathan <116273260+Mcpigeons04@users.noreply.github.com>
2024-08-18 09:39:26 +0530

    Update NewsItem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ const NewsItem=(props)=>{
       <div className='my-3'>
         <div className="card" >
           <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source}</span>
-  <img src={!imageUrl?"https://www.google.com/search?sca_esv=962335f24d61a83d&sca_upv=1&rlz=1C1UEAD_enIN1084IN1084&sxsrf=ADLYWIInsG8VDYU-fjMC8pLG4iERV9jC9Q:1723953827286&q=phone+news&udm=2&fbs=AEQNm0AMrUEM0u25RSHSP2GXBv1FqRTJXslv5T9cWPShXuZK-unDRtidhDD6MO07O664cf3rzCkRGzT6TOmIkWN6z59BEI_sG_WvMHTpzIDOeN0PGxo6GXSSfRa8cDxIQHuP1ScxnvE6gsfZbDnXeIgL3Qyo-XvYLn-hLUVVPKryJo-wirzCOT_YtAyqBqEwc4xdjGdPjKp8-OHxyRpPMvGmyQYg4pWTIw&sa=X&ved=2ahUKEwiy8e-71P2HAxWPxDgGHRhdDmcQtKgLegQIFBAB&biw=1536&bih=695&dpr=1.25#vhid=IoMyxO1Cqb1VuM&vssid=mosaic":imageUrl} className="card-img-top" alt="..."/>
+  <img src={!imageUrl?"https://images.firstpost.com/uploads/2024/06/Vivo-launches-the-X-Fold3-Pro-its-first-foldable-phone-in-India-with-a-carbon-fibre-hinge-check-pricing-2024-06-5409e83d15ee8cc82c81bcaf6982d916-1200x675.jpg?im=FitAndFill=(1200,675)":imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}</p>
